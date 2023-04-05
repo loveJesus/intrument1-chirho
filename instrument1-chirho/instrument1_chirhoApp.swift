389805b4cba884fc8286c9fce0afb980b0172a4c
LoveJesus <loveJesus@loveJes.us>
2023-04-05 16:23:16 -0600

    Initial Commit

---
+++ instrument1-chirho/instrument1_chirhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  instrument1_chirhoApp.swift
+//  instrument1-chirho
+//
+//  Created by Hallelujah on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct instrument1_chirhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ instrument1-chirhoUITests/instrument1_chirhoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  instrument1_chirhoUITestsLaunchTests.swift
+//  instrument1-chirhoUITests
+//
+//  Created by Hallelujah on 4/5/23.
+//
+
+import XCTest
+
+final class instrument1_chirhoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
